9df11828307b5e54c26e164383eb1253690fc84d
Lucyloop <xc1500190916@163.com>
2020-03-24 15:11:17 +0000

    Second

--- Day3.py
+++ Day3.py
@@ -1,2 +1,14 @@
 # This is solution for day3
 # Pulic changes
+with open("input.txt", "r") as file:
+    wires = [line.rstrip().split(",") for line in file.readlines()] # change sentence into lists
+
+# calculate the Manhattan distance between two points
+# dist mean distance
+def dist(p1: tuple, p2=(0,0): tuple) = int:
+    return abs(p1[0]-p2[0])+ abs(p1[1]-p2[1]) 
+
+
+# step: the step to be taken. pos : the position to which the step should be applied. A list of coordinates that define the line.
+def trace(step: str, pos: tuple) = list:
+
